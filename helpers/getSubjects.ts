5415b133eea5d681c3de349ee050af89bc03dfc5
Usef <yosefarafa198@gmail.com>
2025-11-03 14:45:08 +0300

    edit: Update .env file

--- helpers/getSubjects.ts
+++ helpers/getSubjects.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default async function getSubjects() {
     try {
-        const res = await axios.get("http://localhost:3000/api/subjects")
+        const res = await axios.get("https://e-learning-eight-tau.vercel.app/api/subjects")
         return res.data
     } catch (error: any) {
         throw new Error(error.message)
--- hooks/useCurrentUser.ts
+++ hooks/useCurrentUser.ts
@@ -10,7 +10,7 @@ export function useCurrentUser() {
     queryKey: ["current-user", userId],
     queryFn: async () => {
       if (!userId) throw new Error("No token found");
-      const res = await axios.get(`http://localhost:3000/api/auth/users/${userId}`);
+      const res = await axios.get(`https://e-learning-eight-tau.vercel.app/api/auth/users/${userId}`);
       return res.data?.user;
     },
     staleTime: 60_000,
